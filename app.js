fb0853d1d07b5278eaa95cd77d66b92bb8384090
mstaton477 <mlstaton@uncg.edu>
2022-10-19 15:53:01 -0400

    ja

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+app.use(express.static(__dirname + '/pages'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/pages' +'/views/home.html')
+})
+
 // Listen on Port 5000
 app.listen(port, () => console.info(`App listening on port ${port}`))
-
-
-app.use(express.static('pages'));
-
-app.set('home', ',/home');
