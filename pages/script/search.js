7fda172c3a1d34c26414f4071058509cb81e3341
mstaton477 <mlstaton@uncg.edu>
2022-10-20 12:55:54 -0400

    search done

--- pages/script/search.js
+++
@@ -1,9 +0,0 @@
-
-function radioCheck() {
-    if(document.getElementById('search-select').checked){
-        document.getElementById('Answer').style.visibility = 'visible'; 
-    }else{
-        document.getElementById('Answer').style.visibility = 'hidden'; 
-    }
-}
-
