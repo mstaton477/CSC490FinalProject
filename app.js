b0b5090a5c36a2739bf3c5eb1acfe8bdfcec7684
mstaton477 <mlstaton@uncg.edu>
2022-10-26 18:26:29 -0400

    connecting to heroku database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Imports
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -7,6 +8,23 @@ app.use(express.static(__dirname + '/pages'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/pages' +'/views/home.html')
+})
+
+const db = mysql.createConnection({
+    host: "us-cdbr-east-06.cleardb.net", 
+    user: "bab87ea7d060c5", 
+    password: "c593381b", 
+    database: "heroku_209a0a2d6441663"
+});
+
+db.connect((err) => {
+    if(err) {throw err;}
+    console.log("DB connection OK")
+});
+
+db.query("SELECT * from `user`", (err, results) => {
+    if (err) { throw err;}
+    console.log(results); 
 })
 
 // Listen on Port 3000
