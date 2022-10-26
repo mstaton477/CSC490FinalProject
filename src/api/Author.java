4786d2dbe6fe1bbd5c9fc403b4d390e161d70b88
toldge <47599909+toldge@users.noreply.github.com>
2022-10-26 17:09:08 -0400

    Update Author.java
    Add getAuthorByName
    
    OpenLibrary's API makes me sad

--- src/api/Author.java
+++ src/api/Author.java
@@ -1,9 +1,12 @@
 package api;
+
 import org.json.JSONObject;
 
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
 
 public class Author {
+
     private static final APIInterface API = APIInterface.getInstance();
     private String id, name;
 
@@ -23,13 +26,33 @@ public class Author {
         }
     }
 
-    public static Author getAuthor(String _id) {
+    public static Author getAuthorById(String _id) {
         for (Author a : authors) {
             if (a.id.equals(_id)) {
                 return a;
             }
         }
         return new Author(_id);
+    }
+
+    public static LinkedList<Author> getAuthorByName(String _name) {
+        LinkedList<Author> tempAuthorList = new LinkedList<>();
+        for (Author a : authors) {
+            if (a.name.equals(_name)) {
+                tempAuthorList.add(a);
+            }
+        }
+        if (tempAuthorList.size() == 0) try {
+            String s = API.getJsonSearchAsString(RequestType.AUTHORS, _name);
+            new JSONObject(s).getJSONArray("docs").forEach(e -> {
+                if (e instanceof JSONObject) {
+                    tempAuthorList.add(Author.getAuthorById(((JSONObject) e).getString("key")));
+                }
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return tempAuthorList;
     }
 
     public String getName() {
