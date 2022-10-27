668a8183d309aae9d1614177aa3143e9f6dfb1eb
toldge <47599909+toldge@users.noreply.github.com>
2022-10-27 13:51:01 -0400

    Update Connection.java

--- src/connection/Connection.java
+++ src/connection/Connection.java
@@ -1,12 +1,17 @@
 package connection;
 
+import api.Author;
 import api.Book;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.LinkedList;
 
 @SpringBootApplication
 @RestController
@@ -17,6 +22,17 @@ public class Connection {
 
     @GetMapping("/test")
     public String test(@RequestParam(value = "isbn", defaultValue = "9780788789830") String _isbn) {
-        return new JSONObject(Book.getBook(_isbn).getMap()).toString();
+        return Book.getBook(_isbn).toJsonObject().toString();
+    }
+
+    @GetMapping("/author")
+    public String author(@RequestParam(value = "author", defaultValue = "J.R.R. Tolkien") String _author) {
+        HashMap<String, JSONArray> map = new HashMap<>();
+        LinkedList<JSONObject> jsons = new LinkedList<>();
+
+        Author.getAuthorsByName(_author).forEach(e -> jsons.add(e.toJsonObject()));
+
+        map.put("authors", new JSONArray(jsons));
+        return new JSONObject(map).toString();
     }
 }
