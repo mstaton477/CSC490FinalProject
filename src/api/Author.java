7040b61f46c6fa25bece43031161da1f5dd44c02
toldge <47599909+toldge@users.noreply.github.com>
2022-10-27 13:49:47 -0400

    Add toJsonObject

--- src/api/Author.java
+++ src/api/Author.java
@@ -2,6 +2,7 @@ package api;
 
 import org.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 
@@ -35,13 +36,17 @@ public class Author {
         return new Author(_id);
     }
 
-    public static LinkedList<Author> getAuthorByName(String _name) {
+    //NOT STABLE
+    public static LinkedList<Author> getAuthorsByName(String _name) {
+        if (_name == null) return null;
+
         LinkedList<Author> tempAuthorList = new LinkedList<>();
         for (Author a : authors) {
             if (a.name.equals(_name)) {
                 tempAuthorList.add(a);
             }
         }
+
         if (tempAuthorList.size() == 0) try {
             String s = API.getJsonSearchAsString(RequestType.AUTHORS, _name);
             new JSONObject(s).getJSONArray("docs").forEach(e -> {
@@ -49,9 +54,11 @@ public class Author {
                     tempAuthorList.add(Author.getAuthorById(((JSONObject) e).getString("key")));
                 }
             });
+
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+
         return tempAuthorList;
     }
 
@@ -61,5 +68,17 @@ public class Author {
 
     public String getId() {
         return this.id;
+    }
+
+    public JSONObject toJsonObject() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name", this.name);
+        map.put("id", this.id);
+        return new JSONObject(map);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s\tname: %s\tid: %s", super.toString(), this.name, this.id);
     }
 }
