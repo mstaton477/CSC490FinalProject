c808a7404b83f5b5834a2eb8ca90f60966f8575d
mstaton477 <mlstaton@uncg.edu>
2022-11-01 11:27:12 -0400

    starting login

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 // Imports
 const express = require('express');
 const cookieParser = require("cookie-parser");
+const morgan = require('morgan'); 
 const sessions = require('express-session');
 const http = require('http');
 const mysql = require('mysql');
 var bodyParser = require('body-parser');
 const { eachDayOfInterval } = require('date-fns');
+const { response } = require('express');
 const app = express();
 
-// let encodeURL = parseUrl.urlencoded({extended: false});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(sessions({
-    secret: "thisismysecrctekey",
+    secret: 'secret',
     saveUninitialized:true,
     cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 24 hours
     resave: false
 }));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.use(cookieParser());
 
@@ -46,21 +47,6 @@ app.get('/signin', (req, res) => {
 })
 
 
-// app.post('/views/signin', (req, res) => {
-//     const Username = req.body.Username; 
-//     const Name = req.body.Name; 
-//     const Email = req.body.Email; 
-//     const Password = req.body.Password;
-
-
-//     const sql = `INSERT INTO user (Username, Name, Email, Password) VALUES ('${Username}', '${Name}', '${Email}', '${Password}')`;
-//     db.query(sql, function(err, result) {
-//         if(err) throw err; 
-//         console.log('record inserted');
-//         res.redirect('success.html'); 
-//     })
-// })
-
 app.post('/views/signin', function(req, res, next) {
     inputData = {
         Username: req.body.Username,
@@ -84,6 +70,47 @@ app.post('/views/signin', function(req, res, next) {
     
     })
 });
+
+app.get('/login', function(req, res){
+    res.sendFile(__dirname + '/pages' +'/views/login.html')
+})
+
+app.post('/login', function(req, res){
+    let Username = req.body.Username; 
+    let Password = req.body.Password; 
+
+    if(Username && Password){
+        db.query('SELECT * FROM user WHERE Username =? AND Password =?', [Username, Password], function(err, results, fields){
+            if(err){throw err} 
+            if(results.length > 0){
+                req.session.loggedin = true; 
+                req.session.Username = Username; 
+                res.redirect('views/dashboard.html');
+            }else{
+                res.send("Incorrect Username or Password");
+            }
+            res.end(); 
+        });
+    }else{
+        res.send('Please Enter Username and Password');
+        res.end(); 
+    }
+});
+
+app.get('/dashboard', function(req, res, next){
+    if(req.session.loggedinUser){
+        res.render('dashboard',{Username: req.session.Username})
+    }else{
+        res.sendFile(__dirname + '/pages' +'/views/login.html')
+    }
+});
+
+app.get('/logout', function(req, res){
+    req.session.destroy(); 
+    res.redirect('/login');
+});
+
+
 
 app.listen(3000, function () {
     console.log('Node app is running on port 3000');
