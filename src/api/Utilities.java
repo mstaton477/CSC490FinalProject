4b3e003f800d264c9d38a42a612a8bb69e263ddf
toldge <47599909+toldge@users.noreply.github.com>
2022-11-02 15:04:09 -0400

    Update Utilities.java

--- src/api/Utilities.java
+++ src/api/Utilities.java
@@ -1,33 +1,17 @@
 package api;
 
-import java.util.List;
-
 public class Utilities {
-
-    public static String getIndexSafe(String[] _args, int _index) {
-        return _args.length > _index ? _args[_index] : "";
-    }
 
     public static void notNull(Object... _o) throws NullPointerException {
 
-        npe(_o);
+        throwExceptionIfNull(_o);
 
         for (var e : _o) {
-            npe(e);
+            throwExceptionIfNull(e);
         }
     }
 
-    public static void npe(Object _o) throws NullPointerException {
+    public static void throwExceptionIfNull(Object _o) throws NullPointerException {
         if (_o == null) throw new NullPointerException();
-    }
-
-    public static <T extends CharSequence> String flatten(List<T> _list) {
-
-        StringBuilder temp = new StringBuilder();
-
-        for (T str : _list) {
-            temp.append(str);
-        }
-        return temp.toString();
     }
 }
