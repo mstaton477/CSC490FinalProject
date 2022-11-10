aac1d57dd935970953f17fc33bfcdf5324464511
mstaton477 <mlstaton@uncg.edu>
2022-11-09 20:40:09 -0500

    changed port number on app.js

--- app.js
+++ app.js
@@ -200,7 +200,7 @@ app.post('/search', function(req, res){
 
 
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Node app is running on port 3000');
+app.listen(process.env.PORT || 8080, function () {
+    console.log('Node app is running on port 8080');
 });
 
