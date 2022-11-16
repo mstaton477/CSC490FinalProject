79b872566ee91c9e6e507cdb419acacfb20762a5
toldge <47599909+toldge@users.noreply.github.com>
2022-11-16 16:56:30 -0500

    Update getAuthor.js

--- pages/script/getAuthor.js
+++ pages/script/getAuthor.js
@@ -4,8 +4,12 @@ var base_path = './'
 if(typeof base_path === 'undefined') base_path = './pages/script/'
 const getRequest = require(base_path + "getRequest.js")
 
+async function authorHelper(_type, _value, _limit){
+
+}
+
 async function getAuthor(type_, value_, limit_, timeout_){
     if (timeout_ === null) timeout_ = 10000;
-    data = await getRequest(bookHelper,  __timeout, __type, __value, __limit);
+    data = await getRequest(authorHelper,  __timeout, __type, __value, __limit);
     return { "authors": data};
 }
