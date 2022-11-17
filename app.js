073618623cbdcb8c62a3f21fd0fee2fe07dc5bd5
mstaton477 <mlstaton@uncg.edu>
2022-11-17 09:36:36 -0500

    server stuff

--- app.js
+++ app.js
@@ -117,6 +117,10 @@ app.get('/dashboard', function(req, res) {
     }else{
         res.redirect('./login');
     }
+    
+    db.query(`SELECT * FROM book list WHERE Username = ? `, [req.session.loggedinUser], function(error, results, fields){
+        
+    })
 });
 
 // log out function 
